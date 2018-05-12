c1f24fd3b16904f2d82c00cc25fc7b0bea08232d
vpelkonen <ville.pelkonen@qvik.fi>
2018-05-12 23:44:25 +0300

    Fix some file imports and minor issues

--- app/App.js
+++ app/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 
-import ArticleList from './components/ArticleList'
-import articlesData from './fixtures/articlesData.json'
 import AppNavigator from './navigators/AppNavigator'
 
 const App = () => (
