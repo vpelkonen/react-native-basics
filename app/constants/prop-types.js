307a448a4e42416da908a92634009ea2441cc36f
vpelkonen <ville.pelkonen@qvik.fi>
2018-05-13 10:13:43 +0300

    Ignore prop-types.js default export requirement

--- app/constants/prop-types.js
+++ app/constants/prop-types.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 
+/* eslint-disable-next-line import/prefer-default-export */
 export const article = PropTypes.shape({
+  by: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired
